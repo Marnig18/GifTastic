af1c78653723efe9171b34fb4bef12d2b1a689ae
Marni Gross <Marni@Marnis-MBP.fios-router.home>
2017-07-17 22:19:12 -0400

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GifTastic
+
+Site that uses the [giphy api](https://developers.giphy.com/) to display gifs to the page. Contains preset buttons for gif searches as well as takes in user input. 
--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,55 +1,51 @@
 //variables
-
 var topics = ["panda", "hamster", "hedgehog", "cat", "elephant", "dog", "giraffe", "bear", "monkey", "otter", "goat", "penquin", "gorilla", "chimpanzee"];
-var selected ;
-var images=[]
+var selected;
+var images = []
 
 
-
-function createButtons(){
-	for (var j=0; j<topics.length; j++){
+///function to create a button for each topic in topics array 
+function createButtons() {
+	for (var j = 0; j < topics.length; j++) {
 		var button = $("<button>")
 		button.html(topics[j])
 		button.attr("data-name", topics[j])
 		button.addClass("btn")
 		$("#buttons").append(button);
 		console.log(button.attr("data-name"))
+	};
+}
 
- };
-
-}	
+//calling the function 
 createButtons();
 
 
-function onClick(){
-	$(document).on("click", ".btn", getValues); 
+function onClick() {
+	$(document).on("click", ".btn", getValues);
 }
 
 onClick();
 
 
-
-
-
-function getValues(){
-	console.log(this);
+//function to get button value...
+function getValues() {
 	selected = ($(this).attr("data-name"));
-	console.log(selected);
-	var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&api_key=dc6zaTOxFJmzC&q=" + selected 
+	var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&api_key=dc6zaTOxFJmzC&q=" + selected
 
 	reset();
 
-		$.ajax({
-			url: queryURL,
-			method: "GET",
-		}).done(function(response) {
-			console.log( response);
-		///loop to display all results
+	///and run it through the api
+	$.ajax({
+		url: queryURL,
+		method: "GET",
+	}).done(function(response) {
+		console.log(response);
 
+		///loop through api results to display all in individual divs
+		for (var i = 0; i < response.data.length; i++) {
+			var gifs = $("<img>");
 
-		for (var i = 0; i<response.data.length; i++){
-			var gifs = $("<img>");	
-			
+			///adding attributes of links and state to each gif	with responses from data
 			gifs.attr({
 				"src": response.data[i].images.fixed_width_still.url,
 				"data-still": response.data[i].images.fixed_width_still.url,
@@ -57,143 +53,100 @@ function getValues(){
 				"data-state": "still",
 			});
 
+			///adding class to the gifs
 			gifs.addClass("gifs")
+				///appending gifs to the html
 			$("#gifs").append(gifs);
+			///pushing gifs to an images array to ...
 			images.push(gifs);
-			console.log(images);
-		for(var k=0; k<images.length; k++);{
-			var rating = $("<p>")
-			rating.html(response.data[i].rating);
-			$("#gifs").append(rating);
-				}
-					
+			///loop through them to display their ratings
+			for (var k = 0; k < images.length; k++); {
+				var rating = $("<p>")
+				rating.html(response.data[i].rating);
+				$("#gifs").append(rating);
 			}
-
-
-animation();
-		});
-
-
-		
-
-	};
-
-	 
-
-
-//create buttons loop
-
-
-
-$("#button").on("click", function(event){
-				event.preventDefault();
-			 	selected = $("#userInput").val().trim();
-				console.log(selected)
-				var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&api_key=dc6zaTOxFJmzC&q=" + selected 
-
-	reset();
-
-	
-				
-			$.ajax({
-			url: queryURL,
-			method: "GET",
-		}).done(function(response) {
-			console.log( response);
-
-		for (var i = 0; i<response.data.length; i++){
-			var gifs = $("<img>");	
-			gifs.attr({
-				"src": response.data[i].images.fixed_width_still.url,
-				"data-still": response.data[i].images.fixed_width_still.url,
-				"data-animate": response.data[i].images.fixed_width.url,
-				"data-state": "still",
-			});
-			gifs.addClass("gifs")
-
-			$("#gifs").append(gifs);
-			images.push(gifs);
-			console.log(images);
-		for(var k=0; k<images.length; k++);{
-			var rating = $("<p>")
-			rating.html(response.data[i].rating);
-			$("#gifs").append(rating);
-				}
-			}
-
-				animation();
-			
-		
-
-		});
+		}
+		///calling animation function to animate the the gifs on click
+		animation();
 	});
-
-
-
-
-function animation(){
-
- $(".gifs").on("click", function() {
-
- 	var state = $(this).attr("data-state")
-		if(state === "still"){
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate")
-        }
-        else{
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-});
-
 };
 
 
+/// On click function for submit button for user input
+$("#button").on("click", function(event) {
+	event.preventDefault();
+	///getting value of user input and use it for the api query
+	selected = $("#userInput").val().trim();
+	var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&api_key=dc6zaTOxFJmzC&q=" + selected
+
+	reset();
+////Api call
+	$.ajax({
+		url: queryURL,
+		method: "GET",
+	}).done(function(response) {
+		console.log(response);
+
+		///looping through the response to display images to the screen
+		for (var i = 0; i < response.data.length; i++) {
+			var gifs = $("<img>");
+			///add ing link and state attributes to each gif 
+			gifs.attr({
+				"src": response.data[i].images.fixed_width_still.url,
+				"data-still": response.data[i].images.fixed_width_still.url,
+				"data-animate": response.data[i].images.fixed_width.url,
+				"data-state": "still",
+			});
+			///adding class to the gifs
+			gifs.addClass("gifs")
+			///appending gifs to html
+			$("#gifs").append(gifs);
+			///pushing gifs to image array...
+			images.push(gifs);
+		///to loop through and display the rating for each
+			for (var k = 0; k < images.length; k++); {
+				var rating = $("<p>")
+				rating.html(response.data[i].rating);
+				$("#gifs").append(rating);
+			}
+		}
+		//calling animation function to animate each gif on click
+		animation();
+	});
+});
 
 
-function reset(){
+///Function the animate gifs on click
+function animation() {
+
+	$(".gifs").on("click", function() {
+		///grabbing state value for gif clicked
+		var state = $(this).attr("data-state")
+		///if state is still...
+		if (state === "still") {
+			 ///switch gif src to its animated link ...
+			 $(this).attr("src", $(this).attr("data-animate"));
+			 ///and set state varaible to animate
+			$(this).attr("data-state", "animate")
+		///if state variable is set to animate
+		} else {
+			/// set gif src to it still link
+			$(this).attr("src", $(this).attr("data-still"));
+			///set data-state to still
+			$(this).attr("data-state", "still");
+		}
+	});
+};
+
+
+//// reset function to clear gifs on the page after each click and to clear user input field
+function reset() {
 	$("#gifs").html("")
 
-		$("#userInput").val(" ");
+	$("#userInput").val(" ");
 
 }
 
 
 
-
 // user input
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
